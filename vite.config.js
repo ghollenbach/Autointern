9e2a941db51aabbdd6f519046cd065900ed2f3ea
Grace Hollenbach <llamaladycreates@gmail.com>
2025-03-19 18:15:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Autointern/', // Add this line with your repository name
   test: {
     globals: true,
     environment: 'jsdom',
   },
 })
+
