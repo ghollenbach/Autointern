ee839af1ac0016d0c6384bfed6d1425f77e9260e
Grace Hollenbach <llamaladycreates@gmail.com>
2025-03-19 22:33:37 -0400

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,12 @@
 import './home.css';
 
+const Home = () => {
+  return <h1>Welcome to the Home Page!</h1>;
+};
+
+export default Home;
+
+/*
 function Home() {
     return (
         <div className="container">
