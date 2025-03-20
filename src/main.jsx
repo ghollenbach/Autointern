9ddc1a4687405ce28441ba6101a8f2313966a9bd
Grace Hollenbach <llamaladycreates@gmail.com>
2025-03-19 22:27:40 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Import your styles here if you have any
+import App from './App'; // Make sure you have the App component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
