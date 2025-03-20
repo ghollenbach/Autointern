2c0b519650b5fefd8e41540a66f78906abd244c5
Grace Hollenbach <llamaladycreates@gmail.com>
2025-03-20 11:31:13 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import Home from './Home';
-import Use from './Use';
+import Home from './Home.jsx';
+import Use from './Use.jsx';
 
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
